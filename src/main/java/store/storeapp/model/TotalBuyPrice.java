2d45ebcfb79ed5470260b416135f640e047d1640
JeongSu Park <bechalrang@gmail.com>
2024-11-09 11:34:51 +0900

    feat(TotalBuyPrice, TotalBuyPriceTest, README): `총구매액` 객체 추가
    - `총구매액` 객체 추가
    - 해당하는 테스트 코드 추가
    - 해당하는 기능 목록 체크

---
+++ src/main/java/store/storeapp/model/TotalBuyPrice.java
@@ -0,0 +1,58 @@
+package store.storeapp.model;
+
+import store.storeapp.model.value.Won;
+
+public final class TotalBuyPrice {
+
+    private Won won;
+
+    public TotalBuyPrice() {
+        this.won = Won.of(0);
+    }
+
+    public TotalBuyPrice(Won won) {
+        if (won == null) {
+            TotalBuyPriceException.CANNOT_BE_INITIALIZED_TO_NULL_VALUE.raise();
+        }
+        this.won = won;
+    }
+
+    public Won add(Won other) {
+        if (other == null) {
+            TotalBuyPriceException.NULL_CANNOT_BE_ENTERED.raise();
+        }
+        won = won.add(other);
+        return won;
+    }
+
+    public Won won() {
+        return won;
+    }
+
+    @Override
+    public String toString() {
+        return won.toString();
+    }
+
+    private enum TotalBuyPriceException {
+
+        CANNOT_BE_INITIALIZED_TO_NULL_VALUE(() -> {
+            throw new IllegalArgumentException("총구매액은 null로 초기화할 수 없습니다.");
+        }),
+        NULL_CANNOT_BE_ENTERED(() -> {
+            throw new IllegalArgumentException("null이 입력될 수 없습니다.");
+        });
+
+        private final Runnable thrown;
+
+        TotalBuyPriceException(Runnable thrown) {
+            this.thrown = thrown;
+        }
+
+        private void raise() {
+            this.thrown.run();
+        }
+
+    }
+
+}
