9abc29550780cee64c8e721bb999d0b1b69c138a
JeongSu Park <bechalrang@gmail.com>
2024-11-10 15:28:44 +0900

    feat(YesOrNo): `여부` 표현 이넘객체 추가

---
+++ src/main/java/store/storeapp/value/YesOrNo.java
@@ -0,0 +1,25 @@
+package store.storeapp.value;
+
+public enum YesOrNo {
+
+    YES("Y", true),
+    NO("N", false);
+
+    private final String representation;
+    private final boolean value;
+
+    YesOrNo(String representation, boolean value) {
+        this.representation = representation;
+        this.value = value;
+    }
+
+    public static YesOrNo findByRepresentation(String representation) {
+        for (YesOrNo field : YesOrNo.values()) {
+            if (field.representation.equalsIgnoreCase(representation)) {
+                return field;
+            }
+        }
+        throw new IllegalArgumentException("잘못된 입력입니다. 다시 입력해 주세요.");
+    }
+
+}
