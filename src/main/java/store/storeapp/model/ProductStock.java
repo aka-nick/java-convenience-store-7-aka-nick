6b57c8b8038f6d298b3ee3290fef673088ab0ed4
JeongSu Park <bechalrang@gmail.com>
2024-11-10 14:05:26 +0900

    feat(ProductStock): `전체상품재고` 객체 추가

---
+++ src/main/java/store/storeapp/model/ProductStock.java
@@ -0,0 +1,20 @@
+package store.storeapp.model;
+
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Stream;
+import store.storeapp.model.value.ProductName;
+
+public class ProductStock {
+
+    private final Map<ProductName, Product> stock;
+
+    public ProductStock(Map<ProductName, Product> stock) {
+        this.stock = Map.copyOf(stock);
+    }
+
+    public Stream<Entry<ProductName, Product>> entryStream() {
+        return stock.entrySet().stream();
+    }
+
+}
