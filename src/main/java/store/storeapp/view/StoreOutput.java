00eff48aca7c11f9bd46c688b3d5374d309b9d6f
JeongSu Park <bechalrang@gmail.com>
2024-11-10 23:54:49 +0900

    refactor(StoreOutput): `상점출력`의 `재고출력`의 가독성을 위해 정렬함

--- src/main/java/store/storeapp/view/StoreOutput.java
+++ src/main/java/store/storeapp/view/StoreOutput.java
@@ -1,8 +1,10 @@
 package store.storeapp.view;
 
+import java.util.Comparator;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
 import store.storeapp.model.Product;
+import store.storeapp.model.ProductQuantity;
 import store.storeapp.model.ProductStock;
 import store.storeapp.model.Receipt;
 
@@ -26,6 +28,7 @@ public class StoreOutput {
 
     private static String collectProductStockMessage(ProductStock productStock) {
         return productStock.entryStream().map(Entry::getValue)
+                .sorted(Comparator.comparing(Product::name))
                 .map(product -> {
                     if (product.promotion().isEmpty()) {
                         return formatInRegularOf(product);
@@ -41,11 +44,20 @@ public class StoreOutput {
     }
 
     private static String formatInRegularAndPromotionOf(Product product) {
+        String formattedPromotionQuantity = countQuantityOrDispayedOutOfStock(product.promotionQuantity());
+        String formattedRegularQuantity = countQuantityOrDispayedOutOfStock(product.regularQuantity());
         return """
-                - %s %s원 %s개 %s
-                - %s %s원 %s개""".formatted(
-                product.name(), product.price(), product.promotionQuantity(), product.promotion().getName(),
-                product.name(), product.price(), product.regularQuantity());
+                - %s %s원 %s %s
+                - %s %s원 %s""".formatted(
+                product.name(), product.price(), formattedPromotionQuantity, product.promotion().getName(),
+                product.name(), product.price(), formattedRegularQuantity);
+    }
+
+    private static String countQuantityOrDispayedOutOfStock(ProductQuantity productQuantity) {
+        if (productQuantity.intValue() == 0) {
+            return "재고 없음";
+        }
+        return productQuantity + "개";
     }
 
 }
